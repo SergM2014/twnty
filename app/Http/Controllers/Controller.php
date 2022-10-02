0d51f441620ac108e31635ebd729791c9c36c8c1
SergM2014 <weisse011@gmail.com>
2022-10-02 11:04:58 +0300

    preliminary running variant of User swagger implementation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,15 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *   title="My APIs",
+ *   version="1.0.0",
+ *   @OA\Contact(
+ *     email="weisse011@gmail.com"
+ *   )
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
