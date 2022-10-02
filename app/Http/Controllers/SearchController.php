a4f0173e1b54f521a227f54d8cc12816df41eaab
SergM2014 <weisse011@gmail.com>
2022-10-02 14:59:08 +0300

    searchcontroller is swaggerised

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -14,6 +14,48 @@ class SearchController extends Controller
 
     public function __construct(private readonly TaskRepositoryInterface $taskRepository) {}
 
+    /**
+     * @OA\Get(
+     *     path="/api/search",
+     *     summary="Serching Tasks",
+     *     operationId="searchTasks",
+     *     tags={"search"},
+     *     @OA\Parameter(
+     *         in="query",
+     *         name="status",
+     *         @OA\Schema(
+     *           type="string"
+     *      )
+     *     ),
+     *      @OA\Parameter(
+     *         in="query",
+     *         name="executor",
+     *         @OA\Schema(
+     *           type="string"
+     *      )
+     *     ),
+     *   @OA\Response(
+     *      response=200,
+     *      description="successful operation",
+     *      @OA\JsonContent(
+     *          type="array",
+     *          @OA\Items(ref="#/components/schemas/Task")
+     *      ),
+     *      @OA\XmlContent(
+     *          type="array",
+     *          @OA\Items(ref="#/components/schemas/Task")
+     *      )
+     *     ),
+     *     @OA\Response(
+     *         response="500",
+     *         description="unexpected error"
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="not found task error",
+     *     )
+     * )
+     */
     public function __invoke(Request $request): JsonResponse
     {
         $tasks = $this->taskRepository->search();
